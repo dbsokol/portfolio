79b509f1ed0e8b564dd010334cbfa19f8d7f6ae4
dbsokol <github@danielbsokol.engineer>
2023-01-21 10:01:17 -0800

    minor fix

--- static/general/js/loader.js
+++ static/general/js/loader.js
@@ -6,10 +6,7 @@ $(document).ready(function(){
     $('#loader').fadeOut(1000, function(){
       $('resume').show();
       $('footer').show();
-      console.log('in timeout script');
     });
     
   }, 2000);
 });
-
-console.log('loader.js file');
