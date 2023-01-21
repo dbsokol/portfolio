9c6d86cffe5a14ff33a6e1608b38e8e553356b19
dbsokol <github@danielbsokol.engineer>
2023-01-21 09:58:54 -0800

    minor fix

--- general/static/general/js/loader.js
+++ general/static/general/js/loader.js
@@ -6,7 +6,10 @@ $(document).ready(function(){
     $('#loader').fadeOut(1000, function(){
       $('resume').show();
       $('footer').show();
+      console.log('in timeout script');
     });
     
   }, 2000);
 });
+
+console.log('loader.js file');
--- static/general/js/loader.js
+++ static/general/js/loader.js
@@ -1,12 +1,15 @@
 /* global $ */
 
-// $(document).ready(function(){
-//   // $('resume').hide();
-//   setTimeout(function(){
-//     $('#loader').fadeOut(1000, function(){
-//       $('resume').show();
-//       $('footer').show();
-//     });
+$(document).ready(function(){
+  // $('resume').hide();
+  setTimeout(function(){
+    $('#loader').fadeOut(1000, function(){
+      $('resume').show();
+      $('footer').show();
+      console.log('in timeout script');
+    });
     
-//   }, 2000);
-// });
+  }, 2000);
+});
+
+console.log('loader.js file');
